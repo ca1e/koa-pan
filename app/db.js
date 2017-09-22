f343b7c7af4f43864e3cf47fd27e46c955e56daa
Mekolrz <ninekan@qq.com>
2017-09-22 11:36:14 +0800

    Update db.js

--- app/db.js
+++ app/db.js
@@ -3,18 +3,16 @@ import { db } from './config'
 
 mongoose.Promise = global.Promise
 
-const dbOptions = {db: { native_parser: true },
-  server: { poolSize: 5 },replset: { rs_name: 'myReplicaSetName' },
+const dbOptions = {
+  useMongoClient: true,
   user: db.user, pass: db.pass
 }
 
 let moo = mongoose.connect(db.DB_URL, dbOptions)
 
-moo.connection.on('connected',
-() => { console.log(`Mongoose connection open to ${db.DB_URL}`)})
-moo.connection.on('error',
-(err) => { console.log(`Mongoose connection error: ${err}`)})
-moo.connection.on('disconnected',
-() => { console.log('Mongoose connection disconnected')})
+moo.then(
+  () => { console.log(`Mongoose connection open to ${db.DB_URL}`)},
+  (err) => { console.log(`Mongoose connection error: ${err}`)}
+)
 
 export default moo
